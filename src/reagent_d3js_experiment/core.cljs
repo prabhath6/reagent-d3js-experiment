34772fa121575dee41d26e7e4ae1b3a754ff218e
prabhath6 <prabhathkiran20@gmail.com>
2018-09-07 23:20:42 -0500

    Completed input components

--- src/reagent_d3js_experiment/core.cljs
+++ src/reagent_d3js_experiment/core.cljs
@@ -3,10 +3,53 @@
       [reagent.core :as r]))
 
 ;; -------------------------
+;; State
+
+(defonce controllers (r/atom {:m 12 :n 100 :v 2}))
+
+;; -------------------------
+;; Helpers
+(defn string->int [value]
+  (if (clojure.string/blank? value)
+    0
+    (js/parseInt value)))
+
+;; -------------------------
+;; Handlers
+
+(defn tadpole-handler [id value]
+  [:input {:type "text"
+           :value value
+           :class "controller-input"
+           :id id
+           :on-change (fn [e]
+                        (let [input-value (.. e -target -value)
+                              converted-value (string->int input-value)]
+                          (swap! controllers assoc id converted-value)))}])
+
+;; -------------------------
+;; Components
+
+(defn form-component []
+  [:form {:class :form}
+   [:ul
+    [:li
+     [:label {:class :m} "m: "]
+     [tadpole-handler :m (get @controllers :m)]]
+    [:li
+     [:label {:class :n} "n: "]
+     [tadpole-handler :n (get @controllers :n)]]
+    [:li
+     [:label {:class :v} "v: "]
+     [tadpole-handler :v (get @controllers :v)]]]])
+
+;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [:div
+   [:h2 "Welcome to Reagent"]
+   [form-component]])
 
 ;; -------------------------
 ;; Initialize app
